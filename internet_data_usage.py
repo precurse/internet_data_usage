e4dccc513dfff2e64328ce8cda118cc2cf4b4503
Andrew Klaus <andrewklaus@gmail.com>
2015-10-24 16:41:52 -0600

    Updating objects and cleaning code

--- internet_data_usage.py
+++ internet_data_usage.py
@@ -6,80 +6,58 @@ import getpass
 import requests # Handling HTTP Requests and cookies
 from bs4 import BeautifulSoup
 
-__version__ = "0.1"
-__author__ = 'Andrew Klaus'
-__author_email__ = 'andrewklaus@gmail.com'
-__copyright__ = '2015'
+__version__ = "0.1-HEAD"
+__author__ = "Andrew Klaus"
+__author_email__ = "andrewklaus@gmail.com"
+__copyright__ = "2015"
 
 
 def main():
     parser = argparse.ArgumentParser()
 
-    carriers = ('telus_wireline','koodo_mobile')
-    items = ('usage','allotment','plan','days_left','all')
-    output = ('zabbix','terminal')
+    carriers = ("telus_wireline","koodo_mobile")
+    items = ("usage","allotment","plan","days_left","all")
+    output = ("zabbix","terminal")
 
-    parser.add_argument('username', help='Username for account access')
-    parser.add_argument('-p', '--password', default=None, help='Password for account access (Optional: will prompt if required)')
-    parser.add_argument('-c', '--carrier', default='telus', help='Carrier to query from (default=telus)', choices=carriers)
-    parser.add_argument('-i', '--item', default='usage', help='Item to request (default=usage)', choices=items)
-    parser.add_argument('-o', '--output', default='terminal', help='Type of output (default=terminal)', choices=output)
-    parser.add_argument('-t', '--cache_time', default=0, help='Seconds to keep cached items (default=0)')
-
+    parser.add_argument("username", help="Username for account access")
+    parser.add_argument("-p", "--password", default=None, help="Password for account access (Optional: will prompt if required)")
+#    parser.add_argument("-c", "--carrier", default='telus', help='Carrier to query from (default=telus)', choices=carriers)
+#    parser.add_argument("-i", "--item", default='usage', help='Item to request (default=usage)', choices=items)
+#    parser.add_argument("-t", "--cache_time", default=0, type=int, help='Seconds to keep data cached (default=0)')
+    parser.add_argument("-a", "--http_user_agent", default="Mozilla/5.0 (X11; Linux x86_64)")
+#    parser.add_argument("-o", "--output", default='terminal', help='Type of output (default=terminal)', choices=output)
     args = parser.parse_args()
 
+    # Get password if none was specified
     if args.password is None:
         password = getpass.getpass(prompt='Carrier password (will not echo): ')
     else:
         password = args.password
 
-    headers = {'user-agent': 'Mozilla/5.0 (X11; Linux x86_64)' }
-    login_post = "https://www.telus.com/sso/UI/Login?realm=telus&service=telus&locale=en"
-    data_pg = "https://www.telus.com/my-account/usage/overview/usage?INTCMP=USSLeftNavLnkUsage"
-
-    data = { "goto": "https://www.telus.com/my-account/usage/overview/usage?INTCMP=USSLeftNavLnkUsage",
-        "encoded": "false",
-        "service": "telus",
-        "realm": "telus",
-        "portal": "telus",
-        "userLanguage": "en",
-        "IDToken1": args.username,
-        "IDToken2": password,
-        "remember-me-checkbox[]" : ""}
-
-    scraper = TelusScraper("Telus","Telus",data,login_post, data_pg, headers)
+    scraper = TelusWirelineScraper(args.username, password, args.http_user_agent)
 
     scraper.go()
 
     print scraper.get_data_usage()
 
-    #s = requests.session()
-    #s.post(login_post, data, headers=headers)
-
-    #r = s.get(data_pg, headers=headers)
-
-    #print r.text.encode("utf8")
-
-    #page = BeautifulSoup(r.text.encode("utf8"))
-
-    #usage = page.find(class_="used")
-    #cycle = page.find(class_="meters-bill-cycle")
-    #plan = page.find(class_="usage-plan-header usage-type-header")
-
-    #print usage.string.strip()
 
 class CarrierUsageScraper(object):
     """
     name = name of the carrier
     description = description of the carrier
     post_data = HTTP POST data that is passed to the url_login script. Dictionary format.
-    http_header = HTTP Header to use for the website scraping
+    http_headers = HTTP Header to use for the website scraping
 
     url_login = login script that handles the POST authentication and cookies
     url_data = web page that presents the data to parse (post-login)
 
     """
-    def __init__(self, name, description, post_data, url_login, url_data, http_headers):
+    def __init__(self, name,
+                 description,
+                 post_data,
+                 url_login,
+                 url_data,
+                 http_headers):
         self.name = name
         self.description = description
         self.post_data = post_data 
@@ -124,7 +102,28 @@ class CarrierUsageScraper(object):
         self.parse(page)
 
 
-class TelusScraper(CarrierUsageScraper):
+class TelusWirelineScraper(CarrierUsageScraper):
+
+    def __init__(self, username, password, user_agent):
+        name = "TelusWireline"
+        description = "Telus Wireline Services (Internet + TTV)"
+        url_login = "https://www.telus.com/sso/UI/Login?realm=telus&service=telus&locale=en"
+        url_data = "https://www.telus.com/my-account/usage/overview/usage?INTCMP=USSLeftNavLnkUsage"
+
+        post_data = { "goto": "https://www.telus.com/my-account/usage/overview/usage?INTCMP=USSLeftNavLnkUsage",
+        "encoded": "false",
+        "service": "telus",
+        "realm": "telus",
+        "portal": "telus",
+        "userLanguage": "en",
+        "IDToken1": username,
+        "IDToken2": password,
+        "remember-me-checkbox[]" : ""}
+
+        http_headers = {'user-agent': user_agent }
+
+        CarrierUsageScraper.__init__(self, name, description, post_data, url_login, url_data, http_headers)
+
     def parse(self, parse_page):
         usage = parse_page.find(class_="used")
         self._usage = usage.string.strip()
